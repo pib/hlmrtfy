f75b2c1bfa84ed9f7cab134833a9d4b2c67ed8ee
Paul Bonser <pbonser@enthought.com>
2012-04-24 11:56:08 -0500

    Expand the message a bit

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,3 @@
 jQuery(function($) {
-    speak('Yo dawg, I heard you like text-to-speech.');
+    speak("Yo dawg, I heard you like text-to-speech, so I'm working on this site that will read other sites for you.");
 });
